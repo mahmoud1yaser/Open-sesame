5fbdb42d7c22df6deb281c7b30deed1217d67fd9
Ahmed Elsarta <ahmedelsarta411@gmail.com>
2022-12-24 21:39:34 +0200

    imgs refresh with input
    now images refresh with the input, and I added a default feature image to be loaded before user gives an input

--- static/js/audio_recording.js
+++ static/js/audio_recording.js
@@ -6,15 +6,15 @@ var rec; 							//Recorder.js object
 var input; 							//MediaStreamAudioSourceNode we'll be recording
 //soft-coded stuff
 let recordLength_ms = 2000; //variable that controls the length of the recorded sample
-let left_img_path = "../static/assets/Feature_visuals.png" //path for left img
-let mid_img_path = "../static/assets/melspec.png" //path for mid img
+let left_figure_path = "../static/assets/Feature_visuals.png" //path for left img
+let mid_figure_path = "../static/assets/melspec.png" //path for mid img
 
 // shim for AudioContext when it's not avb.
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext //audio context to help us record
 var recordButton = document.getElementById("recordButton");
-var left_infographic = document.getElementById("left-figure")
-var mid_infographic = document.getElementById("mid-figure")
+var left_figure = document.getElementById("left-figure")
+var mid_figure = document.getElementById("mid-figure")
 var message_box = document.getElementById("message")
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
@@ -127,6 +127,36 @@ function createDownloadLink(blob) {
 	// recordingsList.appendChild(li); //commented, used for testing
 }
 
+//function that updates the message textbox
+const display_message = (txt) => {
+	//display message from request into the website
+	message_box.innerHTML=txt;
+}
+
+
+//function for updating the upper left figure and the middle figure 
+const update_figures = () => {
+	// // create a new timestamp     
+	// var timestamp = new Date().getTime(); 
+	// //update the left figure    	
+	// left_figure.src = "../static/assets/Feature_visuals.png?t=" + timestamp; 
+	update_element(left_figure,left_figure_path)
+	update_element(mid_figure, mid_figure_path)
+}
+
+const update_element = (imgElement, imgURL) => {
+	 // create a new timestamp 
+	 var timestamp = new Date().getTime();  
+	 var queryString = "?t=" + timestamp;    
+	 imgElement.src = imgURL + queryString;    
+}
+
+
+
+// left_figure.src = "../static/assets/sound_waveform.png"
+// left_figure.src = left_figure_path
+// // $("#left-figure").prop("src", "Feature_visuals.png?" + new Date().valueOf());
+// mid_figure.src= mid_figure_path
 // function openDoor(){
 // 	console.log("opening the door")
 // 	door_closed_img.style.display = 'none'
@@ -138,16 +168,3 @@ function createDownloadLink(blob) {
 // 	door_closed_img.style.display = 'block'
 // 	door_open_img.style.display = 'none'
 // }
-
-//function that updates the message textbox
-const display_message = (txt) => {
-	//display message from request into the website
-	message_box.innerHTML=txt;
-}
-
-
-//function for updating the upper left figure and the middle figure 
-const update_figures = () => {
-	left_infographic.src = left_img_path
-	mid_infographic.src= mid_img_path
-}
